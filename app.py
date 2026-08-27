f765abca299f16a5cbb7ba4c750597de215fc995
Miqsa0 <onurhasanerol48@gmail.com>
2026-08-27 09:15:01 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,36 +1,15 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, send_from_directory
+import os
 
 app = Flask(__name__)
 
-# Şarkıların YouTube video ID'leri
 PLAYLIST = [
-    {
-        
-        "id": "8uxt-FnNy2I",
-        "title": "Coldplay - Don't Panic",
-        "uploader": "Coldplay",
-        "url": "https://www.youtube.com/watch?v=8uxt-FnNy2I"
-    },
-
-    {
-        "id": "egQqHqad_UM",
-        "title": "Son Feci Bisiklet - Beni Rahatta Dinleyin",
-        "uploader": "Son Feci Bisiklet",
-        "url": "https://www.youtube.com/watch?v=egQqHqad_UM"
-    },
-
-    {
-        "id": "q7mlB-adMBc",
-        "title": "Coldplay - Yellow",
-        "uploader": "Coldplay",
-        "url": "https://www.youtube.com/watch?v=q7mlB-adMBc"
-    },
-    {
-        "id": "PFW2uSCZ0uE",
-        "title": "Coldplay - Clocks",
-        "uploader": "Coldplay",
-        "url": "https://www.youtube.com/watch?v=PFW2uSCZ0uE"
-    },
+    {"file": "falanfilan.mp3", "title": "Falan Filan", "display_title": "Redd - Falan Filan", "uploader": "Redd"},
+    {"file": "dontpanic.mp3", "title": "Don't Panic", "display_title": "Coldplay - Don't Panic", "uploader": "Coldplay"},
+    {"file": "benirahattadinleyin.mp3", "title": "Beni Rahatta Dinleyin", "display_title": "Son Feci Bisiklet - Beni Rahatta Dinleyin", "uploader": "Son Feci Bisiklet"},
+    {"file": "yellow.mp3", "title": "Yellow", "display_title": "Coldplay - Yellow", "uploader": "Coldplay"},
+    {"file": "instantcrush.mp3", "title": "Instant Crush", "display_title": "Daft Punk - Instant Crush", "uploader": "Daft Punk"},
+    {"file": "clocks.mp3", "title": "Clocks", "display_title": "Coldplay - Clocks", "uploader": "Coldplay"}
 ]
 
 @app.route('/')
@@ -40,6 +19,25 @@ def index():
 @app.route('/api/playlist')
 def get_playlist():
     return jsonify(PLAYLIST)
+
+@app.route('/api/photos')
+def get_photos():
+    photos_dir = os.path.join(app.root_path, 'photos')
+    if not os.path.exists(photos_dir):
+        return jsonify([])
+    
+    valid_extensions = ('.jpeg', '.jpg', '.png', '.webp')
+    files = [f for f in os.listdir(photos_dir) if f.lower().endswith(valid_extensions)]
+    files.sort()
+    return jsonify(files[:4])
+
+@app.route('/musics/<path:filename>')
+def serve_musics(filename):
+    return send_from_directory('musics', filename)
+
+@app.route('/photos/<path:filename>')
+def serve_photos(filename):
+    return send_from_directory('photos', filename)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
